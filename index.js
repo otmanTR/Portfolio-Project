ce98b5a0296b900bb308a27a8bcd786ba6585bcf
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-01-03 09:47:50 +0100

    Add js file

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-let hamburger= document.getElementById("hamburger");
-let l1 = document.getElementById("l1");
-let l2 = document.getElementById("l2");
-let l3 = document.getElementById("l3");
+document.getElementById('hamburger').onclick = () => {
+  document.getElementById('navulitems').classList.add('hamburger');
+  document.getElementById('hamburger').classList.add('hamx');
+  document.getElementById('x').classList.remove('hamx');
+};
+
+document.getElementById('x').onclick = () => {
+  document.getElementById('navulitems').classList.remove('hamburger');
+  document.getElementById('hamburger').classList.remove('hamx');
+  document.getElementById('x').classList.add('hamx');
+};
+
+document.getElementById('l1').onclick = () => {
+  document.getElementById('navulitems').classList.remove('hamburger');
+  document.getElementById('hamburger').classList.remove('hamx');
+  document.getElementById('x').classList.add('hamx');
+};
+
+document.getElementById('l2').onclick = () => {
+  document.getElementById('navulitems').classList.remove('hamburger');
+  document.getElementById('hamburger').classList.remove('hamx');
+  document.getElementById('x').classList.add('hamx');
+};
+
+document.getElementById('l3').onclick = () => {
+  document.getElementById('navulitems').classList.remove('hamburger');
+  document.getElementById('hamburger').classList.remove('hamx');
+  document.getElementById('x').classList.add('hamx');
+};
