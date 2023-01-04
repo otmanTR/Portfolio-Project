6322c5cf0696630415b8fd19fd4f3645ae8fbb7f
otmanTR <r.brk.otman@gmail.com>
2023-01-04 23:26:28 +0300

    updated js

--- index.js
+++ index.js
@@ -27,3 +27,20 @@ document.getElementById('l3').onclick = () => {
   document.getElementById('hamburger').classList.remove('hamx');
   document.getElementById('x').classList.add('hamx');
 };
+
+const workCards = [
+  { 
+    title:'Tonic';
+    featured: ['CANOPY', 'Back End Dev', '2022'],
+    description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent';   
+    image: '.tonicmobileimage.png';
+    technologies: ['html', 'css', 'javascript'];
+    linkLive:'https://otmantr.github.io/Portfolio-Project/';
+    linkSource:'https://github.com/otmanTR/Portfolio-Project';
+  }
+]
+
+document.querySelector('.btn1').onclick = () => {
+
+}
+
