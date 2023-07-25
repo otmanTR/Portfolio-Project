4928523ee73e5e2af4dac0c7f48b7b76de843514
otmanTR <r.brk.otman@gmail.com>
2023-07-25 21:30:35 +0300

    update css

--- index.js
+++ index.js
@@ -162,7 +162,8 @@ function renderProjects(projects) {
   const projectsContainer = document.querySelector('.projects-container');
 
   projects.forEach((project, index) => {
-    const projectCard = `
+
+    const projectCard =  `
       <section class="cards">
         <div class="projects project${index + 1}">
           <img
@@ -180,12 +181,14 @@ function renderProjects(projects) {
             <div class="project-button-below">
               <ul class="dev-list">
                 ${project.technologies
-    .map((tech) => `<li><h3 class="html">${tech}</h3></li>`)
+    .map((tech) => `<li><p class="html">${tech}</p></li>`)
     .join('')}
               </ul>
               
-        
-              <button class="btn">See Project</button>
+        <div class="button-container">
+              <a class="project-link btn"  href="${project.liveLink}"> Live Demo </a> 
+              <a class="project-link btn"  href="${project.githubLink}">  GitHub </a>
+              </div>
             </div>
         </div>
         </div>
@@ -215,4 +218,15 @@ skillHeaders.forEach((header) => {
     collapsibleList.classList.toggle('collapsed');
   });
 });
+
+// Download Resume //
+/* eslint-disable-next-line no-unused-vars */
+function downloadResume() {
+  const link = document.createElement('a');
+  link.href = 'https://docs.google.com/document/d/1r8H9oN_eZ-HXQJHezmwv-3DKtJ19UsyYxbKch2S_M7I/edit';
+  link.download = 'Recep_Burak_Otman_Resume.pdf';
+
+  // Trigger the download by programmatically clicking the link
+  link.dispatchEvent(new MouseEvent('click'));
+}
 
