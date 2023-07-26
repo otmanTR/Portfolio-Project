d20960e2c9ddc503093e3047d069abc9b7230624
otmanTR <r.brk.otman@gmail.com>
2023-07-26 10:14:01 +0300

    fix linters

--- index.js
+++ index.js
@@ -29,141 +29,21 @@ document.getElementById('l3').onclick = () => {
   document.getElementById('x').classList.add('hamx');
 };
 
-/* pop-up-starts */
-/* Array */
-
-// const workCards = [
-//   {
-//     title: 'To Do List',
-//     featured: ['CANOPY', 'Front End Dev', '2023'],
-//     description: 'It is a program which you could make checklist for your daily program and clear your list efficiently.',
-//     image: 'do list.png',
-//     technologies: ['html', 'css', 'javascript'],
-//     linkLive: 'otmantr.github.io/To-Do-List/dist',
-//     linkSource: 'git@github.com:otmanTR/To-Do-List.git',
-//   },
-//   {
-//     title: 'Multi-Post Stories',
-//     featured: ['CANOPY', 'Back End Dev', '2022'],
-//     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-//     image: 'destopimage.png',
-//     technologies: ['html', 'css', 'javascript'],
-//     linkLive: 'https://otmantr.github.io/Portfolio-Project/',
-//     linkSource: 'https://github.com/otmanTR/Portfolio-Project',
-//   },
-//   {
-//     title: 'Tonic',
-//     featured: ['CANOPY', 'Back End Dev', '2022'],
-//     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-//     image: 'tonicmobileimage1.png',
-//     technologies: ['html', 'css', 'javascript'],
-//     linkLive: 'https://otmantr.github.io/Portfolio-Project/',
-//     linkSource: 'https://github.com/otmanTR/Portfolio-Project',
-//   },
-//   {
-//     title: 'Multi-Post Stories',
-//     featured: ['CANOPY', 'Back End Dev', '2022'],
-//     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-//     image: 'multimobileimage1.png',
-//     technologies: ['html', 'css', 'javascript'],
-//     linkLive: 'https://otmantr.github.io/Portfolio-Project/',
-//     linkSource: 'https://github.com/otmanTR/Portfolio-Project',
-//   },
-// ];
-
-// document.getElementById('btn').onclick = () => {
-//   document.getElementById('card-container').classList.add('star');
-
-//   document.getElementById('headerliner').classList.add('blur');
-
-//   document.getElementById('Portfolio').classList.add('blur');
-//   document.getElementById('About').classList.add('blur');
-//   document.getElementById('Contact').classList.add('blur');
-//   document.getElementById('cardL1').innerText = workCards[0].technologies[0];
-//   document.getElementById('cardL2').innerHTML = workCards[0].technologies[1];
-//   document.getElementById('cardL3').innerHTML = workCards[0].technologies[2];
-//   document.getElementById('mobiletonic').src = workCards[0].image;
-//   document.getElementById('site').href = workCards[0].linkLive;
-//   document.getElementById('source').href = workCards[0].linkSource;
-// };
-
-// document.getElementById('tonicx').onclick = () => {
-//   document.getElementById('card-container').classList.remove('star');
-
-//   document.getElementById('headerliner').classList.remove('blur');
-
-//   // document.getElementById('body').classList.remove('blur');
-
-//   document.getElementById('Portfolio').classList.remove('blur');
-//   document.getElementById('About').classList.remove('blur');
-//   document.getElementById('Contact').classList.remove('blur');
-// };
-
-// document.getElementById('btn1').onclick = () => {
-//   document.getElementById('card-container').classList.add('star');
-
-//   document.getElementById('headerliner').classList.add('blur');
-
-//   document.getElementById('Portfolio').classList.add('blur');
-//   document.getElementById('About').classList.add('blur');
-//   document.getElementById('Contact').classList.add('blur');
-//   document.getElementById('cardL1').innerHTML = workCards[1].technologies[0];
-//   document.getElementById('cardL2').innerHTML = workCards[1].technologies[1];
-//   document.getElementById('cardL3').innerHTML = workCards[1].technologies[2];
-//   document.getElementById('mobiletonic').src = workCards[1].image;
-//   document.getElementById('site').href = workCards[1].linkLive;
-//   document.getElementById('source').href = workCards[1].linkSource;
-// };
-
-// document.getElementById('btn2').onclick = () => {
-//   document.getElementById('card-container').classList.add('star');
-
-//   document.getElementById('headerliner').classList.add('blur');
-
-//   document.getElementById('Portfolio').classList.add('blur');
-//   document.getElementById('About').classList.add('blur');
-//   document.getElementById('Contact').classList.add('blur');
-//   document.getElementById('cardL1').innerHTML = workCards[2].technologies[0];
-//   document.getElementById('cardL2').innerHTML = workCards[2].technologies[1];
-//   document.getElementById('cardL3').innerHTML = workCards[2].technologies[2];
-//   document.getElementById('mobiletonic').src = workCards[2].image;
-//   document.getElementById('site').href = workCards[2].linkLive;
-//   document.getElementById('source').href = workCards[2].linkSource;
-// };
-
-// document.getElementById('btn3').onclick = () => {
-//   document.getElementById('card-container').classList.add('star');
-
-//   document.getElementById('headerliner').classList.add('blur');
-
-//   document.getElementById('Portfolio').classList.add('blur');
-//   document.getElementById('About').classList.add('blur');
-//   document.getElementById('Contact').classList.add('blur');
-//   document.getElementById('cardL1').innerHTML = workCards[1].technologies[0];
-//   document.getElementById('cardL2').innerHTML = workCards[1].technologies[1];
-//   document.getElementById('cardL3').innerHTML = workCards[1].technologies[2];
-//   document.getElementById('mobiletonic').src = workCards[3].image;
-//   document.getElementById('site').href = workCards[1].linkLive;
-//   document.getElementById('source').href = workCards[1].linkSource;
-// };
 /* contact validation */
 document.getElementById('contactForm').onsubmit = (e) => {
   const email = document.getElementById('formEmail').value;
   if (email !== email.toLowerCase()) {
     e.preventDefault();
     document.getElementById('error-message').innerHTML = 'Email must be in lowercase';
-  
+  }
 };
-};
-
 
 // Render Projects
 function renderProjects(projects) {
   const projectsContainer = document.querySelector('.projects-container');
 
   projects.forEach((project, index) => {
-
-    const projectCard =  `
+    const projectCard = `
       <section class="cards">
         <div class="projects project${index + 1}">
           <img
@@ -197,7 +77,6 @@ function renderProjects(projects) {
 
     projectsContainer.innerHTML += projectCard;
   });
-  
 }
 
 // Fetch Projects
@@ -205,7 +84,7 @@ fetch('projects.json')
   .then((response) => response.json())
   .then((data) => renderProjects(data));
 
-  // Get all the skill_header elements
+// Get all the skill_header elements
 const skillHeaders = document.querySelectorAll('.skill_header');
 
 // Add click event listener to each header
@@ -229,4 +108,3 @@ function downloadResume() {
   // Trigger the download by programmatically clicking the link
   link.dispatchEvent(new MouseEvent('click'));
 }
-
