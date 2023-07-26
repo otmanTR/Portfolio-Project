3613d6283a8066aca42ce880a372987d43f6ef30
otmanTR <r.brk.otman@gmail.com>
2023-07-26 16:08:45 +0300

    change required

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-/* eslint-disable prefer-destructuring */
-document.getElementById('hamburger').onclick = () => {
-  document.getElementById('navulitems').classList.add('hamburger');
-  document.getElementById('hamburger').classList.add('hamx');
-  document.getElementById('x').classList.remove('hamx');
-};
+// /* eslint-disable prefer-destructuring */
+// document.getElementById('hamburger').onclick = () => {
+//   document.getElementById('navulitems').classList.add('hamburger');
+//   document.getElementById('hamburger').classList.add('hamx');
+//   document.getElementById('x').classList.remove('hamx');
+// };
 
-document.getElementById('x').onclick = () => {
-  document.getElementById('navulitems').classList.remove('hamburger');
-  document.getElementById('hamburger').classList.remove('hamx');
-  document.getElementById('x').classList.add('hamx');
-};
+// document.getElementById('x').onclick = () => {
+//   document.getElementById('navulitems').classList.remove('hamburger');
+//   document.getElementById('hamburger').classList.remove('hamx');
+//   document.getElementById('x').classList.add('hamx');
+// };
 
-document.getElementById('l1').onclick = () => {
-  document.getElementById('navulitems').classList.remove('hamburger');
-  document.getElementById('hamburger').classList.remove('hamx');
-  document.getElementById('x').classList.add('hamx');
-};
+// document.getElementById('l1').onclick = () => {
+//   document.getElementById('navulitems').classList.remove('hamburger');
+//   document.getElementById('hamburger').classList.remove('hamx');
+//   document.getElementById('x').classList.add('hamx');
+// };
 
-document.getElementById('l2').onclick = () => {
-  document.getElementById('navulitems').classList.remove('hamburger');
-  document.getElementById('hamburger').classList.remove('hamx');
-  document.getElementById('x').classList.add('hamx');
-};
+// document.getElementById('l2').onclick = () => {
+//   document.getElementById('navulitems').classList.remove('hamburger');
+//   document.getElementById('hamburger').classList.remove('hamx');
+//   document.getElementById('x').classList.add('hamx');
+// };
 
-document.getElementById('l3').onclick = () => {
-  document.getElementById('navulitems').classList.remove('hamburger');
-  document.getElementById('hamburger').classList.remove('hamx');
-  document.getElementById('x').classList.add('hamx');
-};
+// document.getElementById('l3').onclick = () => {
+//   document.getElementById('navulitems').classList.remove('hamburger');
+//   document.getElementById('hamburger').classList.remove('hamx');
+//   document.getElementById('x').classList.add('hamx');
+// };
 
 /* contact validation */
 document.getElementById('contactForm').onsubmit = (e) => {
